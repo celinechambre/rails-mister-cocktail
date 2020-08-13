30717a8b973a7105b8fde453bc31ed4b54dafb29
Celine Chambre <chambre.celine@gmail.com>
2020-08-13 14:57:12 +0200

    challenge 3

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,2 +1,36 @@
 class DosesController < ApplicationController
+  before_action :set_dose, only: [:new, :create]
+
+  def new
+    @dose = Dose.new
+  end
+
+  def create
+
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to cocktail_path(@dose.cocktail)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+
+    redirect_to cocktail_path(@dose.cocktail)
+  end
+
+  private
+
+  def set_dose
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
+
 end
